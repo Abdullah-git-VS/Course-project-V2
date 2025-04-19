9b3b4ace995f974778052782d1cfd8b9acb521d9
Hmeshal <meshal.h.alossaimi@gmail.com>
2025-04-19 15:39:47 +0300

    change style of sign in

--- customer/register.php
+++ customer/register.php
@@ -39,10 +39,154 @@ if(isset($_POST['submit'])){
       <!-- custom css file link  -->
       <link rel="stylesheet" href="css/style.css">
       <style>
-         input{
-            text-align: center;
+         
+         *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
          }
+
+         body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+         }
+
+         header {
+	         display:flex;
+	         justify-content:space-between;
+	         align-items:center;
+	         padding:10px;
+         	width:100%;
+            background-color: #252557;
+           }
+
+           .navbar {
+            display:flex;
+            justify-content:space-between;
+            align-items:center;
+            padding:10px;
+            background-color: #252557;
+            width:100%;
+            height: 60px;
+           }
+
+           .logo {
+            display:flex;
+            align-items:center;
+            color:white;
+            font-size:30px;
+            font-weight:bold;
+           }
+
+             .logo p {
+            margin-left: 800px;
+            border-radius: 5px;
+             }
+
+             .logo h1 {
+               font-size:30px;
+	            text-transform:uppercase;
+             }
+
+             .logo a {
+               color:white;
+               text-decoration:none;
+               font-size:20px;
+               transition:0.3 ease;
+               
+             }
+
+             .logo a:hover {
+               background-color:rgba(255, 255, 255, 0.21);
+             }
+
+               .form-container {
+                  background-color: #272757;
+                   width: 450px;
+                   height: 450px;
+                   margin: 100px auto;
+                     padding: 20px;
+                    border-radius: 20px;
+
+                     display: flex;
+                      justify-content: center;
+                      align-items: center;
+               }
+
+               form {
+                  width: 100%;
+                  height: 500px;
+                    border-radius: 20px;
+                   display: flex;
+                   flex-wrap: wrap;
+                    gap: 5px;
+               }
+
+               .form-container form h3 {
+                  color: black;
+                  font-size: 30px;
+                  
+               }
+
+               .form-container form .inputs {
+                  margin: 10px;
+                  width: 300px;
+                  height: 50px;
+                  border-radius: 5px;
+                  background-color: rgb(43, 43, 115);
+                  border: none;
+                  outline: none;
+                  color: white;
+                  padding: 20px;
+                  /* the input size will grow if there is a space for grow */
+                  flex-grow: 1;
+               }
+
+               .form-container form .inputs::placeholder {
+                  color: white;
+                  font-size: 20px;
+               }
+
+               .form-container form .submitting {
+                  background-color: rgb(43, 43, 115);
+                  color: white;
+                  border: none;
+                  border-radius: 5px;
+                  position: relative;
+                  bottom: 3px;
+                  margin: 5px;
+                  cursor: pointer;
+                  width: 200px;
+                  height: 50px;
+                  flex-grow: 1;
+                  font-size: 20px;
+
+                  transition: 0.3s ease;
+               }
+
+               .form-container form .submitting:hover {
+                  background-color: rgb(126, 126, 161);
+                  transition: 0.3s;
+               }
+
+
+               .form-container form p,a {
+                  color: black;
+                  font-size: 20px;
+                  text-decoration: none;
+               }
+               
+
+               
+
+         
+
          </style>
+
+
          <script src="../script.js"></script>
 </head>
 <body>
@@ -53,21 +197,30 @@ if(isset($message)){
       echo '<div class="message" onclick="this.remove();">'.$message.'</div>';
    }
 }
+  
 ?>
    
+<header>
+    <nav class="navbar">
+      <div class="logo">
+        <h1>Transport</h1>
+        <p> <a href="#">back</a></p>
+      </div>
+    </nav>
+ </header>
+
    <div class="form-container">
-      
+
       <form onSubmit="return validate();" method="post" >
-         <h3>انشاء حساب جديد</h3>
-         <input type="text" name="name" required placeholder="اسم المستخدم" class="box">
-         <input type="email" name="email" required placeholder="البريد الالكتروني" class="box">
-         <input type="password" name="password" required placeholder="كلمة المرور" class="box" name="password" id="password">
-         <input type="password" name="cpassword" required placeholder="تأكيد كلمة المرور" class="box" id="confirm_password">
-         <input type="submit" name="submit" class="btn" value="تسجيل حساب">
+         <h3 class="heading">انشاء حساب جديد</h3>
+         <input class="inputs" type="text" name="name" required placeholder="اسم المستخدم" class="box">
+         <input class="inputs" type="email" name="email" required placeholder="البريد الالكتروني" class="box">
+         <input class="inputs" type="password" name="password" required placeholder="كلمة المرور" class="box" name="password" id="password">
+         <input class="inputs" type="password" name="cpassword" required placeholder="تأكيد كلمة المرور" class="box" id="confirm_password">
+         <input class="submitting" type="submit" name="submit" class="btn" value="تسجيل حساب">
          <p>هل لديك حساب؟ <a href="../../home_Page.php"> تسجيل دخول</a></p>
-      </form>
-      
+      </form>      
    </div>
-   
+
 </body>
 </html>
