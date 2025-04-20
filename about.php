53afe174c80a0ebf368d4d1efc75a24c17a3eff8
stresshack <worthityea200@gmail.com>
2025-04-20 13:25:14 +0300

    hh

--- about.php
+++ about.php
@@ -9,7 +9,7 @@ include('admin\Function\config.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About</title>
+    <title>Tariq is here</title>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
   <link rel="stylesheet" href="../Course-project-V2/hide_style.css">
   <style>
