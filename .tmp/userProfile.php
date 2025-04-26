48dc04caa1b07daa6c329fb70d1a54bca55754b4
Abdullah-git-VS <205581067+dsgdd3@users.noreply.github.com>
2025-04-26 11:29:55 +0300

    recovery userProfile

--- .tmp/userProfile.php
+++ .tmp/userProfile.php
@@ -98,6 +98,74 @@ text-align: left;
 
         .profile-container .profile-content .header img {
             margin: 10px;
+            width: 120px;
+            height: 120px;
+            border-radius: 50%;
+            object-fit: cover;
+            border: 3px solid white;
+            background-color: #fff;
+        }
+
+        .profile-container .profile-content .header h2 {
+            margin: 0;
+            font-size: 24px;
+            color: white;
+        }
+
+        .profile-container .profile-content .details {
+            margin-top: 20px;
+        }
+
+        .profile-container .profile-content .details h3 {
+            margin-right: 400px;
+            color: white;
+        }
+
+        .profile-container .profile-content .details ul {
+            list-style: none;
+            padding: 0;
+            color: white;
+
+            margin-right: 400px;
+            margin-top: 40px;
+        }
+
+        .profile-container .profile-content .details ul li {
+            color: white;
+            padding: 10px;
+            margin-bottom: 10px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            text-align: left;
+        }
+
+        .profile-container .profile-content button {
+            margin-left: 360px;
+            padding: 10px 20px;
+            width: 200px;
+            background-color: #272757;
+        }
+
+        /* .profile-container {
+            max-width: 600px;
+            margin: 50px auto;
+            background-color: #272757;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+        }
+
+        .profile-container .profile-content {
+            border: 1px solid #ccc;
+        }
+
+        .profile-container .profile-content .header {
+            display: flex;
+            align-items: center;
+            gap: 20px;
+        }
+
+        .profile-container .profile-content .header img {
+            margin: 10px;
 
             width: 120px;
             height: 120px;
@@ -122,7 +190,7 @@ text-align: left;
             color: white;
             text-transform: uppercase;
             font-size: 20px;
-            
+
         }
 
         .profile-container .profile-content .details ul {
@@ -149,7 +217,7 @@ text-align: left;
             margin-top: 20px;
             padding: 10px 20px;
             width: 200px;
-            background-color:rgba(58, 29, 160, 0.75);
+            background-color: rgba(58, 29, 160, 0.75);
             color: white;
             border: none;
             border-radius: 5px;
@@ -159,95 +227,49 @@ text-align: left;
             border: 2px solid white;
         }
 
-        .profile-container .profile-content button:hover{
-           background-color:transparent;
+        .profile-container .profile-content button:hover {
+            background-color: transparent;
             transform: scale(1.05);
-            color:rgba(252, 252, 252, 0.59);
+            color: rgba(252, 252, 252, 0.59);
         }
 
-        
-
+        */
     </style>
 </head>
+
 <body>
-   <!-- <nav class="navbar">
+    <nav class="navbar">
         <div class="logo">
             <h1>Profile</h1>
         </div>
     </nav>
-    -->
-    
-    <?php include '../list.php'; ?>
-    <?php  $title = "User Profile";
-          include '../header.php'; ?>
-
-    <div class="profile-container">
-
-        <div class="profile-content">
-
-            <div class="header">
-                <img src="../<?php echo $row["profile_pic"]; ?>">
-                <h2> user name: <?php echo $row["name"]; ?> </h2>
-            </div>
-
-
-            <div class="details">
-                <h3> profile information </h3>
-                <ul>
-                    <li> Email: <?php echo $row["email"]; ?> </li>
-                    <li> Phone: <?php echo $row["phone"]; ?> </li>
-                    <li> Address: <?php echo $row["address"]; ?>
-                    <li>
-                </ul>
-            </div>
-            <button type="submit" name="button"> Edit Profile </button>
+    <form action="" method='post'>
+        <div class="w">
+            <?php
+            $img = $row['profile_pic'];
+            echo "<img src= $img width=100px> <br>"
+            ?>
+            <?php echo "Welcome " . $row['name'] . "<br>"; ?>
         </div>
-    </div>
+        <div class='frame-container'>
+            <div class='info'>
+                <?php echo "Email: " . $row['email'] . "<br>"; ?> <input type='submit' name='changeEmail' value='Change'>
+
+            </div>
+            <div class='info'>
+                <?php echo "Name: " . $row['name'] . "<br>"; ?> <input type='submit' name='changeName' value='Change'>
+
+            </div>
+            <div class='info'>
+                <center>
+                    <button type='submit' name='changePass'>Change password</button>
+                </center>
+            </div>
+        </div>
 
 
+    </form>
 
-
-
-
-
-
-
-
-
-
-
-    <!--
-<form action="" method='post'>
-
-    <div class="w">
-    <?php
-    // $img = $row['profile_pic'];
-    //  echo "<img src= $img width=100px> <br>" 
-    ?>
-    <?php // echo"Welcome " . $row['name'] . "<br>";
-    ?>
-    </div>
-    <div class = 'frame-container'>
-    <div class='info'>
-        <?php  // echo"Email: " . $row['email'] . "<br>";
-        ?>  <input type='submit'  name='changeEmail' value='Change'>
-
-    </div>
-    <div class='info'>
-    <?php  // echo"Name: " . $row['name']. "<br>";
-    ?>  <input type='submit' name='changeName' value='Change'>
-   
-    </div>
-    <div class='info'>
-        <center>
-    <button type='submit'  name='changePass'>Change password</button>
-    </center>
-    </div>
-    </div>
-
-
-</form>
--->
 </body>
 
 </html>
