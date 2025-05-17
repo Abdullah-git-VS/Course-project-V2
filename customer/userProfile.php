f6965d03a2b6e86ff03e07ef73d48a476c874862
Fahad <moqfah97@gmail.com>
2025-05-17 20:51:20 +0300

    1

--- customer/userProfile.php
+++ customer/userProfile.php
@@ -6,6 +6,18 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: ../shared/homePage.php");
     exit;
 }
+if (isset($_GET['logout'])) {
+  unset($user_id);
+  session_destroy();
+
+  if (file_exists('../shared/homePage.php')) {
+    header('Location: ../shared/homePage.php');
+    exit;
+  } else {
+    header('Location: ../shared/homePage.php');
+    exit;
+  }
+};
 $select = mysqli_query($con, "SELECT * FROM `user_info` WHERE `id` = $userID");
 $row =  mysqli_fetch_array($select);
 
