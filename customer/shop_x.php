e0763750b8b2303f8819c300a63ca02801cda7a6
Hmeshal <meshal.h.alossaimi@gmail.com>
2025-05-19 10:12:16 +0300

    delete all duplicated css in customer

--- customer/shop_x.php
+++ customer/shop_x.php
@@ -59,7 +59,7 @@ mysqli_close($con);
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>عربة التسوق</title>
-   <link rel="stylesheet" href="css/style.css">
+   
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
    
 </head>
