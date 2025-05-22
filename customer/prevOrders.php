b29265fed43d11daec2d6df1801001f3a8a4750f
Fahad <moqfah97@gmail.com>
2025-05-22 19:43:51 +0300

    body

--- customer/prevOrders.php
+++ customer/prevOrders.php
@@ -1,4 +1,15 @@
 <!-- include header  -->
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   </head>
+   <body>
+    
+   </body>
+   </html>
    <?php
     $title = "Past orders";
     // include($_SERVER["DOCUMENT_ROOT"] . "\shared\header.php");
@@ -43,7 +54,7 @@ if (isset($_GET['logout'])) {
     while ($row = mysqli_fetch_assoc($res) ) {
     echo "<div style='background-color:#272757;width:50%; margin:0 auto;margin-top:20px;'>";  
     echo "<span style='font-size:25px;'> Product: </span> " . $row['product'] . "<br>";
-    "<span style='font-size:25px;'> Price: </span> " . $row['price'] . "<br>";
+    echo"<span style='font-size:25px;'> Price: </span> " . $row['price'] . "<br>";
     echo "<span style='font-size:25px;'>Vehicle: </span>" . $row['vehicle'] . "<br>";
     echo "<span style='font-size:25px;'>Destination: </span> " . $row['destination'] . "<br>";
     echo "</div>";
@@ -59,23 +70,3 @@ if (isset($_GET['logout'])) {
     mysqli_close($con);
   ob_end_flush();
     ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-        <link rel="stylesheet" href="<?php echo "http://" . $_SERVER['HTTP_HOST'] . "/shared/css/newStyle.css"; ?>">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Past orders</title>
-    </head>
-    <body>
-    
-   
-
-
-      
-
-    
-      
-    </body>
-    </html>
